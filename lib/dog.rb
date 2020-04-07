7798f9b728ac941f0cb1c765dabb7c1a98cc676e
Matt Fleming <matthewfleming8@gmail.com>
2020-04-07 00:29:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,18 @@
-class Dog 
-  def initialize(name)
-    @name = name
+class Dog
+  def name=(dogs_name)
+    @name = dogs_name
   end
   
   def name
     @name
   end
   
-  def name=(new_name)
-    @name = new_name
-  end
-  
-  fido = Dog.new("Fido")
-  
-
-
+  def breed=(breed)
+    @breed = breed
+ end
+ 
+ def breed
+   @breed
+ end
+ 
+end
